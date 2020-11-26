1e7e71f43e9dddf4d37651f5d60f4fa5dfb2f0e5
choiwaterrim <sxxrxm@gmail.com>
2020-11-27 06:10:47 +0900

    tutorial

--- Assets/script/PanelOpener.cs
+++ Assets/script/PanelOpener.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; //Text, Image 등의UI관련 변수 등을 사용할수 있게됩니다
+using UnityEngine.SceneManagement;
+ 
 public class PanelOpener : MonoBehaviour
 {
-    public GameObject Panel;
-
-    public void OpenPanel()
+    public Image TestIm0; //기존에 존제하는 이미지
+    public Sprite Im1; //바뀌어질 이미지
+    public Sprite Im2;
+    public Sprite Im3;
+    public Sprite Im4;
+    public Sprite Im5;
+    public int count = 0;
+ 
+    public void ChangeImage()
     {
-        if(Panel != null)
-        {
-            Panel.SetActive(true);
+        if(count == 0){
+            TestIm0.sprite = Im1;
+            count ++; //TestImage에 SourceImage를 TestSprite에 존제하는 이미지로 바꾸어줍니다
+        }else if(count == 1){
+            TestIm0.sprite = Im2;
+            count ++;
+        }else if(count == 2){
+            TestIm0.sprite = Im3;
+            count ++;
+        }else if(count == 3){
+            TestIm0.sprite = Im4;
+            count ++;
+        }else if(count == 4){
+            count ++;
+            TestIm0.sprite = Im5;
+        
         }
+        
     }
+
 }
