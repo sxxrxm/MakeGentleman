900c13c85b70d1fd48df31118c1bea0b3a1b75c3
choiwaterrim <sxxrxm@gmail.com>
2020-11-27 14:11:20 +0900

    merge error fix

--- Assets/script/ChangeImage.cs
+++ Assets/script/ChangeImage.cs
@@ -16,7 +16,7 @@ public class ChangeImage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        dataController.SetMoney(10000000);
     }
 
     // Update is called once per frame
